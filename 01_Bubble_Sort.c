c02bfa033a45f88bb2858a7e346273f3c69eeaa7
Aaron Creed P. Celindro <aaroncreedpcelindro@gmail.com>
2025-11-08 01:14:14 +0800

    01

---
+++ 01_Bubble_Sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[] = {4,5,7,6,1,2};
+
+    printf("The unsorted array: \n");
+    for(int i = 0; i < 6; i++){
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    printf("The sorted array using Bubble Sort:\n");
+    for(int i = 0; i < 7; i++){
+        for(int j = 0; j < 6; j++){
+        if(arr[j] > arr[i]){
+            int temp = 0;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        }
+    }
+    for (int j = 0; j < 6; j++){
+        printf("arr[%d] = %d\n", j, arr[j]);
+    }
+
+
+    return 0;
+}
